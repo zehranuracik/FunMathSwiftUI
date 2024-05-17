923e039221986ddae51b08aed81358b90cad5f6b
Zehra Nur Acik <zehranuracik@hotmail.com>
2024-05-17 09:42:49 +0300

    Initial Commit

---
+++ FunMath/FunMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunMathApp.swift
+//  FunMath
+//
+//  Created by Zehra Nur Açık on 17.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct FunMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
